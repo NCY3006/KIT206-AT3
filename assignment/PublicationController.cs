5444f3a4be1795a4f15497d1ee2b45d6c6d2b830
NCY3006 <mcyng@utas.edu.au>
2023-04-27 14:10:55 +1000

    Added

---
+++ assignment/PublicationController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assignment
+{
+    class PublicationController
+    {
+        public static List<string> PublicationYears { get; set; }
+        public static Publication Publications { get; set; }
+    }
+}
---
+++ assignment/ResearcherController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assignment
+{
+    class ResearcherController
+    {
+        public static List<Researcher> Researchers { get; set; }
+        public static Researcher CurrentResearcher { get; set; }
+    }
+
+}
