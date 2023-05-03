773201c6c29eb7b7011c7770f1fa21891cfb6817
CheeseRabit <383468066@qq.com>
2023-05-03 23:01:49 +1000

    1

--- assignment/DataSource/ERDAdapter.cs
+++ assignment/DataSource/ERDAdapter.cs
@@ -45,7 +45,6 @@ namespace RAP.DataSource
         public Researcher[] fetchBasicResearcherDetails()
         {
             List<Researcher> researchers = new List<Researcher>();
-            
             conn.Open();
 
             try
@@ -56,7 +55,7 @@ namespace RAP.DataSource
                 researcherAdapter.Fill(researcherDataSet, "researcher");
                 foreach (DataRow row in researcherDataSet.Tables["researcher"].Rows)
                 {
-                    Console.WriteLine(row["given_name"].ToString());
+                    Console.WriteLine("Researcher name: {0} {1}", row["given_name"], row["family_name"].ToString());
                     Researcher cur = new Researcher();
                     cur.GivenName = row["given_name"].ToString();
                     cur.FamilyName = row["family_name"].ToString();
@@ -183,6 +182,7 @@ namespace RAP.DataSource
                     conn.Close();
                 }
             }
+
         }
 
         public Publication completePublicationDetails(Publication p)
@@ -253,3 +253,152 @@ namespace RAP.DataSource
     }
 }
 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+///<summary>
+/// Illustrates the absolute basics of querying a table in the RAP case study database.
+///</summary>
+namespace MySQLDemo
+{
+
+    class SqlCommandDemo
+    {
+        //Note that ordinarily these would (1) be stored in a settings file and (2) have some basic encryption applied
+        private const string db = "kit206";
+        private const string user = "kit206";
+        private const string pass = "kit206";
+        private const string server = "alacritas.cis.utas.edu.au";
+
+        private MySqlConnection conn;
+
+        public SqlCommandDemo()
+        {
+            //Create the connection object (does not actually make the connection yet)
+            //Note that the HRIS case study database has the same values for its name, user name and password (to keep things simple)
+            string connectionString = String.Format("Database={0};Data Source={1};User Id={2};Password={3}", db, server, user, pass);
+            conn = new MySqlConnection(connectionString);
+        }
+
+        // Calls methods that demonstrate SqlCommand capabilities for reading data
+        static void Main()
+        {
+            SqlCommandDemo demo = new SqlCommandDemo();
+
+            int count = demo.GetNumberOfRecords();
+            Console.WriteLine("Number of researcher records: {0}", count);
+            Console.WriteLine();
+            Console.WriteLine("Names from researcher table:");
+            demo.ReadData();
+            Console.WriteLine();
+            Console.WriteLine("Names read into a DataSet (should be the same as above):");
+            demo.ReadIntoDataSet();
+        }
+
+        /// <summary>
+        /// Using the ExecuteReader method to select from a single table
+        /// </summary>
+        public void ReadData()
+        {
+            MySqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                MySqlCommand cmd = new MySqlCommand("select given_name, family_name from researcher", conn);
+
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    //This illustrates how the raw data can be obtained using an indexer [] or a particular data type can be obtained using a GetTYPENAME() method.
+                    Console.WriteLine("{0} {1}", rdr[0], rdr.GetString(1));
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Using the ExecuteReader method to select from a single table
+        /// </summary>
+        public void ReadIntoDataSet()
+        {
+            try
+            {
+                var researcherDataSet = new DataSet();
+                var researcherAdapter = new MySqlDataAdapter("select * from researcher", conn);
+                researcherAdapter.Fill(researcherDataSet, "researcher");
+
+                foreach (DataRow row in researcherDataSet.Tables["researcher"].Rows)
+                {
+                    //Again illustrating that indexer (based on column name) gives access to whatever data
+                    //type was obtained from a given column, but can call ToString() on an entry if needed.
+                    Console.WriteLine("Researcher name: {0} {1}", row["given_name"], row["family_name"].ToString());
+                }
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Using the ExecuteScalar method
+        /// </summary>
+        /// <returns>number of records</returns>
+        public int GetNumberOfRecords()
+        {
+            int count = -1;
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command
+                MySqlCommand cmd = new MySqlCommand("select COUNT(*) from researcher", conn);
+
+                // 2. Call ExecuteScalar to send command
+                // This convoluted approach is safe since cannot predict actual return type
+                count = int.Parse(cmd.ExecuteScalar().ToString());
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return count;
+        }
+    }
+}
