279fac0f4f5a005c061cebdbe574068da9f9468b
NCY3006 <mcyng@utas.edu.au>
2023-05-03 16:15:59 +1000

    Update Publication.cs
    added age and some public strings

--- assignment/Publication.cs
+++ assignment/Publication.cs
@@ -8,18 +8,21 @@ namespace assignment
 {
     public class Publication
     {
-        public string DOI;
-        public string Title;
-        public string Authors;
-        public Date date;
-        public OutPutType type;
-        public string CiteAs;
-        public Date Available;
+        public string DOI { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        //public Date date;
+        public int PublicationYear { get; set; }
+        public OutPutType type{ get; set; }
+        public string CiteAs { get; set; }
+        public DateTime Available { get; set; }
 
         // calculate the age
         public int Age()
         {
-            return 0;
+            var today = DateTime.Today;
+            var age = today.Year - PublicationYear;
+            return age;
         }
     }
 }
